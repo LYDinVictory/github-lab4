91dc47760c0a09f2d9de666b295151925f069abd
LYDinVictory <lyd_study@163.com>
2015-11-11 23:48:57 +0800

    Create urls.py

---
+++ urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import *
+from mysite.views import *
+urlpatterns = patterns('',
+    (r'^picture/(?P<path>.*)','django.views.static.serve',{'document_root':'./template/'}),
+    (r'^search/$',search),
+    (r'^detail/(.*)/$',detail),
+    (r'^search/Delete/(.*)/$',search_again),
+    (r'^search_Add/$',search_add),  
+    (r'^update/(.*)/$',update),
+)

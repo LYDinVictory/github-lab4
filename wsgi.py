305e2a46f840416d8fbd90dee31c4ec7a2a72f7a
LYDinVictory <lyd_study@163.com>
2015-11-12 13:01:26 +0800

    merged

--- wsgi.py
+++ wsgi.py
@@ -16,3 +16,4 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
 #for C4
+#merge test for C4
